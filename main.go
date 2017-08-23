1b3dc7e14846eca7692115ff4cee3e6c7a447052
Tom Barrett <tbtommyb@gmail.com>
2017-08-23 21:37:26 +0100

    Added header and more detail for each entry

--- main.go
+++ main.go
@@ -7,18 +7,31 @@ import (
   "os"
 )
 
+func printEntry(entry os.FileInfo) (error) {
+  fmt.Printf("%-12s %10d %-20s\n", entry.Mode(), entry.Size(), entry.Name())
+  return nil
+}
+
+func printHeader() (error) {
+  fmt.Printf("%-12s %10s %-20s\n", "Mode", "Size", "Name")
+  return nil
+}
+
 func main() {
   var dirname string
+
   if len(os.Args) == 1 {
     dirname = "."
   } else {
     dirname = os.Args[1]
   }
+
   entries, err := ioutil.ReadDir(dirname)
   if err != nil {
     log.Fatal(err)
   }
+  printHeader()
   for _, entry := range entries {
-    fmt.Println(entry.Name())
+    printEntry(entry)
   }
 }
