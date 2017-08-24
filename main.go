440013b6ad0f459faef8b217af8ceeda581be97c
Tom Barrett <tbtommyb@gmail.com>
2017-08-24 14:01:46 +0100

    Applied gofmt

--- main.go
+++ main.go
@@ -1,71 +1,75 @@
 package main
 
 import (
-  "io/ioutil"
-  "fmt"
-  "log"
-  "os"
-  "github.com/fatih/color"
-  "flag"
+	"flag"
+	"fmt"
+	"github.com/fatih/color"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
 type colorConfig struct {
-  dir (func(a ...interface{}) string)
-  symlink (func(a ...interface{}) string)
+	dir     (func(a ...interface{}) string)
+	symlink (func(a ...interface{}) string)
 }
 
 type fileInfo struct {
-  file os.FileInfo
-  colors *colorConfig
+	file   os.FileInfo
+	colors *colorConfig
 }
 
 func (e *fileInfo) print() {
-  name := e.file.Name()
-  if showColors {
-    if e.file.IsDir() {
-      name = e.colors.dir(name)
-    }
-  }
-  fmt.Printf("%-12s %10d %-20s\n", e.file.Mode(), e.file.Size(), name)
+	name := e.file.Name()
+	if showColors {
+		if e.file.IsDir() {
+			name = e.colors.dir(name)
+		}
+		if e.file.Mode()&os.ModeSymLink != 0 {
+			name = e.colors.symlink(name)
+		}
+	}
+	fmt.Printf("%-12s %10d %-20s\n", e.file.Mode(), e.file.Size(), name)
 }
 
 var (
-  showColors bool
-  dirname string
+	showColors bool
+	dirname    string
 )
 
-func printHeader() (error) {
-  fmt.Printf("%-12s %10s %-20s\n", "Mode", "Size", "Name")
-  return nil
+func printHeader() error {
+	fmt.Printf("%-12s %10s %-20s\n", "Mode", "Size", "Name")
+	return nil
 }
 
 func init() {
-  flag.BoolVar(&showColors, "colors", false, "Toggle colours in output")
-  flag.Parse()
+	flag.BoolVar(&showColors, "colors", false, "Toggle colours in output")
+	flag.Parse()
 
-  dirProvided := flag.NArg() > 0
-  if dirProvided {
-    dirname = flag.Arg(0)
-  } else {
-    dirname = "."
-  }
+	dirProvided := flag.NArg() > 0
+	if dirProvided {
+		dirname = flag.Arg(0)
+	} else {
+		dirname = "."
+	}
 }
 
 func main() {
-  colorConf := &colorConfig{
-    dir: color.New(color.FgYellow).SprintFunc(),
-  }
+	colorConf := &colorConfig{
+		dir:     color.New(color.FgYellow).SprintFunc(),
+		symlink: color.New(color.FgRed).SprintFunc(),
+	}
 
-  entries, err := ioutil.ReadDir(dirname)
-  if err != nil {
-    log.Fatal(err)
-  }
-  printHeader()
-  for _, entry := range entries {
-    file := &fileInfo{
-      file: entry,
-      colors: colorConf,
-    }
-    file.print()
-  }
+	entries, err := ioutil.ReadDir(dirname)
+	if err != nil {
+		log.Fatal(err)
+	}
+	printHeader()
+	for _, entry := range entries {
+		file := &fileInfo{
+			file:   entry,
+			colors: colorConf,
+		}
+		file.print()
+	}
 }
