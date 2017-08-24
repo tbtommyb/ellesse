abb9e8e54137ce9e3a0485a726a392c2aa56cc62
Tom Barrett <tbtommyb@gmail.com>
2017-08-24 14:41:50 +0100

    Truncate filenames based on terminal width to make full use of available space

--- main.go
+++ main.go
@@ -4,9 +4,19 @@ import (
 	"flag"
 	"fmt"
 	"github.com/fatih/color"
+	"golang.org/x/crypto/ssh/terminal"
 	"io/ioutil"
 	"log"
 	"os"
+)
+
+var (
+	showColors bool
+	dirname    string
+	termWidth  int
+	modeWidth  int
+	sizeWidth  int
+	nameWidth  int
 )
 
 type colorConfig struct {
@@ -19,8 +29,16 @@ type fileInfo struct {
 	colors *colorConfig
 }
 
+func truncate(s string, n int) string {
+	length := len(s)
+	if length > n {
+		s = s[:n-3] + "..."
+	}
+	return s
+}
+
 func (e *fileInfo) print() {
-	name := e.file.Name()
+	name := truncate(e.file.Name(), nameWidth)
 	if showColors {
 		if e.file.IsDir() {
 			name = e.colors.dir(name)
@@ -29,20 +47,16 @@ func (e *fileInfo) print() {
 			name = e.colors.symlink(name)
 		}
 	}
-	fmt.Printf("%-12s %10d %-20s\n", e.file.Mode(), e.file.Size(), name)
+	fmt.Printf("%-[1]*[2]s %[3]*[4]d %-[5]*[6]s\n", modeWidth, e.file.Mode(), sizeWidth, e.file.Size(), nameWidth, name)
 }
 
-var (
-	showColors bool
-	dirname    string
-)
-
 func printHeader() error {
-	fmt.Printf("%-12s %10s %-20s\n", "Mode", "Size", "Name")
+	fmt.Printf("%-[1]*[2]s %[3]*[4]s %-[5]*[6]s\n", modeWidth, "Mode", sizeWidth, "Size", nameWidth, "Name")
 	return nil
 }
 
 func init() {
+	var err error
 	flag.BoolVar(&showColors, "colors", false, "Toggle colours in output")
 	flag.Parse()
 
@@ -52,6 +66,14 @@ func init() {
 	} else {
 		dirname = "."
 	}
+
+	termWidth, _, err = terminal.GetSize(0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	modeWidth = 12
+	sizeWidth = 8
+	nameWidth = termWidth - modeWidth - sizeWidth - 2 // -2 to allow for spaces
 }
 
 func main() {
