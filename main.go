4a81a45a5fee4afe4d4b4e12d3a1fdf5b5c8dd60
Thomas Barrett <thomas.barrett@yourgolftravel.com>
2017-09-01 18:25:05 +0100

    Tidying up layout and variable name

--- main.go
+++ main.go
@@ -14,21 +14,37 @@ var (
 	showColors bool
 	dirname    string
 	termWidth  int
-	modeWidth  int
-	sizeWidth  int
+	modeWidth  int = 12
+	sizeWidth  int = 10
 	nameWidth  int
 )
 
+func init() {
+	var err error
+	flag.BoolVar(&showColors, "colors", false, "Toggle colours in output")
+	flag.Parse()
+
+	dirname = flag.Arg(0)
+	if dirname == "" {
+		dirname = "."
+	}
+
+	termWidth, _, err = terminal.GetSize(0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	nameWidth = termWidth - modeWidth - sizeWidth - 2 // -2 to allow for spaces
+}
+
 type colorSprintFunc func(a ...interface{}) string
 
-type colorConfig struct {
+type colorHolder struct {
 	dir     colorSprintFunc
 	symlink colorSprintFunc
 }
 
 func truncate(s string, n int) string {
-	length := len(s)
-	if length > n {
+	if len(s) > n {
 		s = s[:n-3] + "..."
 	}
 	return s
@@ -39,7 +55,7 @@ func outputLine(val1, val2, val3 interface{}) {
 	fmt.Printf("%-[1]*[2]v %[3]*[4]v %-[5]*[6]v\n", modeWidth, val1, sizeWidth, val2, nameWidth, val3)
 }
 
-func printEntry(f os.FileInfo, c *colorConfig) {
+func printEntry(f os.FileInfo, c *colorHolder) {
 	name := truncate(f.Name(), nameWidth)
 	if showColors {
 		if f.IsDir() {
@@ -56,29 +72,8 @@ func printHeader() {
 	outputLine("Mode", "Size", "Name")
 }
 
-// TODO: Move to be with var declaration
-func init() {
-	var err error
-	flag.BoolVar(&showColors, "colors", false, "Toggle colours in output")
-	flag.Parse()
-
-	dirname = flag.Arg(0)
-	if dirname == "" {
-		dirname = "."
-	}
-
-	termWidth, _, err = terminal.GetSize(0)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// TODO: Set constants in the var
-	modeWidth = 12
-	sizeWidth = 10
-	nameWidth = termWidth - modeWidth - sizeWidth - 2 // -2 to allow for spaces
-}
-
 func main() {
-	colorConf := &colorConfig{
+	colors := &colorHolder{
 		dir:     color.New(color.FgYellow).SprintFunc(),
 		symlink: color.New(color.FgRed).SprintFunc(),
 	}
@@ -89,6 +84,6 @@ func main() {
 	}
 	printHeader()
 	for _, entry := range entries {
-		printEntry(entry, colorConf)
+		printEntry(entry, colors)
 	}
 }
